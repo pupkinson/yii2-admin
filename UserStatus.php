2062fd8c28470afe661cd3ec0707f8b752ac61bf
pupkinson <79422502+pupkinson@users.noreply.github.com>
2021-02-28 02:43:59 +0300

    Bug Fix

---
+++ UserStatus.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace mdm\admin\components;
+
+const STATUS_DELETED = 0;
+const STATUS_INACTIVE = 9;
+const STATUS_ACTIVE = 10;
+
+/**
+ * Description of UserStatus
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 2.9
+ */
+class UserStatus
+{
+    const INACTIVE = STATUS_INACTIVE;
+    const ACTIVE = STATUS_ACTIVE;
+    const DELETED = STATUS_DELETED;
+
+}
