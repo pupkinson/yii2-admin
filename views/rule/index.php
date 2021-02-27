76aa1f2e6d8e9ca57183b258d8ef4bf0610254f6
pupkinson <79422502+pupkinson@users.noreply.github.com>
2021-02-27 17:53:22 +0300

    Update index.php
    Made compatible with bootstrap4

--- views/rule/index.php
+++ views/rule/index.php
@@ -30,7 +30,26 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'name',
                 'label' => Yii::t('rbac-admin', 'Name'),
             ],
-            ['class' => 'yii\grid\ActionColumn',],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'buttons' => [
+                    'update' =>  function($url,$model) {
+                        return Html::a('<i class="fas fa-edit"></i>', $url, [
+                            'title' => Yii::t('app', 'update')
+                        ]);
+                    },
+                    'view' =>  function($url,$model) {
+                        return Html::a('<i class="fas fa-eye"></i>', $url, [
+                            'title' => Yii::t('app', 'view')
+                        ]);
+                    },
+                    'delete' => function($url,$model) {
+                        return Html::a('<i class="fas fa-trash"></i>', $url, [
+                            'title' => Yii::t('app', 'delete')
+                        ]);
+                    }
+                 ]
+            ],
         ],
     ]);
     ?>
