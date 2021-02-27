59d11bef161b570829c818fc65f94b5ba85d5b33
pupkinson <79422502+pupkinson@users.noreply.github.com>
2021-02-27 17:52:00 +0300

    Update index.php
    Made compatible with bootstrap4

--- views/item/index.php
+++ views/item/index.php
@@ -43,7 +43,26 @@ unset($rules[RouteRule::RULE_NAME]);
                 'attribute' => 'description',
                 'label' => Yii::t('rbac-admin', 'Description'),
             ],
-            ['class' => 'yii\grid\ActionColumn',],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'buttons' => [
+                    'update' =>  function($url,$model) {
+                        return Html::a('<i class="fas fa-edit"></i>', $url, [
+                            'title' => Yii::t('app', 'update')
+                        ]);
+                    },
+                    'view' =>  function($url,$model) {
+                        return Html::a('<i class="fas fa-eye"></i>', $url, [
+                            'title' => Yii::t('app', 'view')
+                        ]);
+                    },
+                    'delete' => function($url,$model) {
+                        return Html::a('<i class="fas fa-trash"></i>', $url, [
+                            'title' => Yii::t('app', 'delete')
+                        ]);
+                    }
+                 ]
+            ],
         ],
     ])
     ?>
